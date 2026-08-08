481baea46b1b851669efe5fad49a91b6904c66d2
Adlan Daniel <68151150+adaniel22@users.noreply.github.com>
2026-08-08 15:27:21 +0200

    Bemenet-validáció: class-validator a users és login végpontokon

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,15 +1,17 @@
-import {defineConfig} from '@mikro-orm/postgresql';
+import { defineConfig } from '@mikro-orm/postgresql';
 import 'dotenv/config';
 
 export default defineConfig({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    dbName: process.env.DB_NAME,
-    entities: ['dist/**/*.entity.js'],
-    entitiesTs: ['src/**/*.entity.ts'],
-    migrations: {
-        path: 'dist/migrations', pathTs: 'src/migrations',
-    },
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
+  entities: ['dist/**/*.entity.js'],
+  entitiesTs: ['src/**/*.entity.ts'],
+  debug: true,
+  migrations: {
+    path: 'dist/migrations',
+    pathTs: 'src/migrations',
+  },
 });
